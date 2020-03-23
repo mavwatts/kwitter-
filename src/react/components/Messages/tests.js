369f5f3a257855e9792b1ed29f979ad9bd8d926d
Kyle Negley <kpnegley@gmail.com>
2020-03-23 12:23:49 -0400

    got pictures to display on the message board

---
+++ src/react/components/Messages/tests.js
@@ -0,0 +1,6 @@
+// import { useDispatch } from "react-redux";
+// import { receiveUserImage } from "../../../redux/messages";
+
+// const dispatch = useDispatch();
+
+// console.log(dispatch(receiveUserImage("negs123")));
