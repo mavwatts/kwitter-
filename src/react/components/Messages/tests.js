b002aa648b8718af5c7f0cfbd26e383d9c5ff430
Kyle Negley <kpnegley@gmail.com>
2020-03-23 14:14:58 -0400

    deleted test component in messages

--- src/react/components/Messages/tests.js
+++
@@ -1,6 +0,0 @@
-// import { useDispatch } from "react-redux";
-// import { receiveUserImage } from "../../../redux/messages";
-
-// const dispatch = useDispatch();
-
-// console.log(dispatch(receiveUserImage("negs123")));
